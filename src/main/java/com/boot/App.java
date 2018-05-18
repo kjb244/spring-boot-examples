a6d02c7391eb06ffa2d31597db9037aa55039be3
kjb244 <kevin.bacino@gmail.com>
2018-05-17 21:24:54 -0400

    Update App.java

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 /**
- * Hello world!
+ * Hello world
  *
  */
 @SpringBootApplication
